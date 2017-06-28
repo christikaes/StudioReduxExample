b47ca672f0accf157e38290784a8bac412e57915
christikaes <christikaes@gmail.com>
2017-06-28 02:44:27 -0400

    Part5 Undo/Redo

--- store.js
+++ store.js
@@ -1,5 +1,5 @@
 const store = Redux.createStore(
-    reducer,
+    undoable(reducer, { undoType: "UNDO", redoType: "REDO", jumpToPastType: "JUMP_TO_PAST", jumpToFutureType: "JUMP_TO_FUTURE" }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
